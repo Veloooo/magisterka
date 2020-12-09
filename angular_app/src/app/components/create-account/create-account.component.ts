5cc42d5e02c7a99dcbbe5ea25bfbc626f04cd05a
Danielo <nazguleczek@gmail.com>
2020-12-09 15:47:53 +0100

    Update 9.12

---
+++ angular_app/src/app/components/create-account/create-account.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {UserAccount} from '../../model/user-account';
+import {AccountService} from '../../core/account.service';
+import {Router} from '@angular/router';
+
+@Component({
+    selector: 'app-create-account',
+    templateUrl: './create-account.component.html',
+    styleUrls: ['./create-account.component.scss']
+})
+export class CreateAccountComponent implements OnInit {
+
+    newUser: UserAccount;
+    userFraction = new FormGroup({
+        fraction: new FormControl(''),
+    });
+
+    constructor(private accountService: AccountService,
+                private _router: Router) {
+    }
+
+    ngOnInit() {
+    }
+
+    onSubmit() {
+        this.newUser = new UserAccount();
+        this.newUser.fraction = this.userFraction.value['fraction'];
+        this.accountService.finalizeRegister(this.newUser).subscribe(data => this._router.navigate(['main-view'], {replaceUrl: true}));
+    }
+
+}
